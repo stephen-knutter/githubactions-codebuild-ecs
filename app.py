b8d353fd3c424afe986b12ab3cc319e1a18b56a9
stephen <knutter@stephens-MacBook-Air-2.local>
2021-06-08 17:06:26 -0600

    fix file

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 """Main application file"""
 from flask import Flask
-app = Flas(__name__)
+import logging
+app = Flask(__name__)
+
+# Initialize logger
+LOGGER = logging.getLogger()
+LOGGER.setLevel(logging.INFO)
 
 @app.route('/<random_string>')
 def returnBackwardsString(random_string):
     """Reverse and return the provided URI"""
-    return "Breaking the unit test"
-    # return "".join(reversed(random_string))
+    LOGGER.info('Received a message: %s', random_string)
+    return "".join(reversed(random_string))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
